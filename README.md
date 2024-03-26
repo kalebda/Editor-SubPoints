882eb23e2a1b37e77aa200de50b456a27f967c34
kalebda <kalebu246@gmail.com>
2024-03-26 12:33:18 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# Heading Tool
+
+![Version of EditorJS that the plugin is compatible with](https://badgen.net/badge/Editor.js/v2.0/blue)
+
+Provides SubPoints Blocks for the [Editor.js](https://ifmo.su/editor).
+
+## Installation
+
+Get the package
+
+```shell
+yarn add @kalebu2468/editorjs-subpoints
+```
+
+Include module at your application
+
+```javascript
+import SubPoint from "@kalebu2468/editorjs-subpoints";
+```
+
+## Usage
+
+Add a new Tool to the `tools` property of the Editor.js initial config.
+
+```javascript
+var editor = EditorJS({
+  ...
+
+  tools: {
+    ...
+    subpoints: SubPoints,
+  },
+
+  ...
+});
+```
+
+## Shortcut
+
+You can insert this Block by a useful shortcut. Set it up with the `tools[].shortcut` property of the Editor's initial config.
+
+```javascript
+var editor = EditorJS({
+  ...
+
+  tools: {
+    ...
+    subpoints: {
+      class: SubPoints,
+    },
+  },
+
+  ...
+});
+```
+
+## Config Params
+
+All properties are optional.
+
+| Field        | Type       | Description                   |
+| ------------ | ---------- | ----------------------------- |
+| placeholder  | `string`   | subpoint's placeholder string |
+| levels       | `number[]` | enabled heading levels        |
+| defaultLevel | `number`   | default heading level         |
+
+```javascript
+var editor = EditorJS({
+  ...
+
+  tools: {
+    ...
+    subpoints: {
+      class: SubPoints,
+      config: {
+        placeholder: 'Enter a subpoint',
+        levels: [4],
+        defaultLevel: 4
+      }
+    }
+  }
+
+  ...
+});
+```
+
+## Tool's settings
+
+![An image showing the subpoint block tool](https://capella.pics/634ad545-08d7-4cb7-8409-f01289e0e5e1.jpg)
+
+You can select one of six levels for heading.
+
+## Output data
+
+| Field | Type     | Description                                        |
+| ----- | -------- | -------------------------------------------------- |
+| text  | `string` | subpoint's text                                    |
+| level | `number` | level of subpoint: 1 for H1, 2 for H2 ... 6 for H6 |
+
+```json
+{
+  "type": "subpoints",
+  "data": {
+    "text": "Why Telegram is the best messenger",
+    "level": 4
+  }
+}
+```
---
+++ src/index.js
@@ -0,0 +1,433 @@
+/**
+ * Build styles
+ */
+import "./index.css";
+
+/**
+ * @typedef {object} HeaderData
+ * @description Tool's input and output data format
+ * @property {string} text — Header's content
+ * @property {number} level - Header's level from 1 to 6
+ */
+
+/**
+ * @typedef {object} HeaderConfig
+ * @description Tool's config from Editor
+ * @property {string} placeholder — Block's placeholder
+ * @property {number[]} levels — Heading levels
+ * @property {number} defaultLevel — default level
+ */
+
+/**
+ * Header block for the Editor.js.
+ *
+ * @author CodeX (team@ifmo.su)
+ * @copyright CodeX 2018
+ * @license MIT
+ * @version 2.0.0
+ */
+export default class SubPoints {
+  /**
+   * Render plugin`s main Element and fill it with saved data
+   *
+   * @param {{data: HeaderData, config: HeaderConfig, api: object}}
+   *   data — previously saved data
+   *   config - user config for Tool
+   *   api - Editor.js API
+   */
+  constructor({ data, config, api }) {
+    this.api = api;
+
+    /**
+     * Styles
+     *
+     * @type {object}
+     */
+    this._CSS = {
+      block: this.api.styles.block,
+      wrapper: "ce-header",
+    };
+    /**
+     * Tool's settings passed from Editor
+     *
+     * @type {HeaderConfig}
+     * @private
+     */
+    this._settings = config;
+
+    /**
+     * Block's data
+     *
+     * @type {HeaderData}
+     * @private
+     */
+    this._data = this.normalizeData(data);
+    /**
+     * Main Block wrapper
+     *
+     * @type {HTMLElement}
+     * @private
+     */
+    this._element = null;
+  }
+  /**
+   * Normalize input data
+   *
+   * @param {HeaderData} data - saved data to process
+   *
+   * @returns {HeaderData}
+   * @private
+   */
+  normalizeData(data) {
+    const newData = {};
+
+    if (typeof data !== "object") {
+      data = {};
+    }
+
+    newData.text = data.text || "";
+    newData.level = parseInt(data.level) || this.defaultLevel.number;
+
+    return newData;
+  }
+
+  /**
+   * Return Tool's view
+   *
+   * @returns {HTMLHeadingElement}
+   * @public
+   */
+  render() {
+    if (
+      this.api.blocks.getCurrentBlockIndex() == -1 ||
+      (this.api.blocks.getCurrentBlockIndex() > -1 &&
+        this.api.blocks.getBlockByIndex(this.api.blocks.getCurrentBlockIndex())
+          .name == "points")
+    ) {
+      this._element = this.getTag();
+      return this._element;
+    } else {
+      return this.getNoNode();
+    }
+  }
+
+  /**
+   * Method that specified how to merge two Text blocks.
+   * Called by Editor.js by backspace at the beginning of the Block
+   *
+   * @param {HeaderData} data - saved data to merger with current block
+   * @public
+   */
+  merge(data) {
+    console.log("----------->", data);
+    const newData = {
+      text: this.data.text + data.text,
+      level: this.data.level,
+    };
+
+    this.data = newData;
+  }
+
+  /**
+   * Validate Text block data:
+   * - check for emptiness
+   *
+   * @param {HeaderData} blockData — data received after saving
+   * @returns {boolean} false if saved data is not correct, otherwise true
+   * @public
+   */
+  validate(blockData) {
+    return blockData.text.trim() !== "";
+  }
+
+  /**
+   * Extract Tool's data from the view
+   *
+   * @param {HTMLHeadingElement} toolsContent - Text tools rendered view
+   * @returns {HeaderData} - saved data
+   * @public
+   */
+  save(toolsContent) {
+    return {
+      text: toolsContent.innerHTML,
+      level: this.currentLevel.number,
+    };
+  }
+
+  /**
+   * Allow Header to be converted to/from other blocks
+   */
+  static get conversionConfig() {
+    return {
+      export: "text", // use 'text' property for other blocks
+      import: "text", // fill 'text' property from other block's export string
+    };
+  }
+
+  /**
+   * Sanitizer Rules
+   */
+  static get sanitize() {
+    return {
+      level: false,
+      text: {},
+    };
+  }
+
+  /**
+   * Get current Tools`s data
+   *
+   * @returns {HeaderData} Current data
+   * @private
+   */
+  get data() {
+    this._data.text = this._element.innerHTML;
+    this._data.level = this.currentLevel.number;
+
+    return this._data;
+  }
+
+  /**
+   * Store data in plugin:
+   * - at the this._data property
+   * - at the HTML
+   *
+   * @param {HeaderData} data — data to set
+   * @private
+   */
+  set data(data) {
+    this._data = this.normalizeData(data);
+
+    /**
+     * If level is set and block in DOM
+     * then replace it to a new block
+     */
+    if (data.level !== undefined && this._element.parentNode) {
+      /**
+       * Create a new tag
+       *
+       * @type {HTMLHeadingElement}
+       */
+      const newHeader = this.getTag();
+
+      /**
+       * Save Block's content
+       */
+      newHeader.innerHTML = this._element.innerHTML;
+
+      /**
+       * Replace blocks
+       */
+      this._element.parentNode.replaceChild(newHeader, this._element);
+
+      /**
+       * Save new block to private variable
+       *
+       * @type {HTMLHeadingElement}
+       * @private
+       */
+      this._element = newHeader;
+    }
+
+    /**
+     * If data.text was passed then update block's content
+     */
+    if (data.text !== undefined) {
+      this._element.innerHTML = this._data.text || "";
+    }
+  }
+
+  /**
+   * Get tag for target level
+   * By default returns second-leveled header
+   *
+   * @returns {HTMLElement}
+   */
+  getTag() {
+    /**
+     * Create element for current Block's level
+     */
+    const tag = document.createElement(this.currentLevel.tag);
+
+    /**
+     * Add text to block
+     */
+    tag.innerHTML = this._data.text || "";
+
+    /**
+     * Add styles class
+     */
+    tag.classList.add(this._CSS.wrapper);
+
+    /**
+     * Add Placeholder
+     */
+    tag.dataset.placeholder = this.api.i18n.t(this._settings.placeholder || "");
+
+    return tag;
+  }
+
+  getNoNode() {
+    /**
+     * Create element
+     */
+    const tag = document.createElement("DIV");
+
+    /**
+     * Add '' to block
+     */
+    tag.innerHTML = "";
+
+    return tag;
+  }
+
+  /**
+   * Get current level
+   *
+   * @returns {level}
+   */
+  get currentLevel() {
+    let level = this.levels.find(
+      (levelItem) => levelItem.number === this._data.level
+    );
+
+    if (!level) {
+      level = this.defaultLevel;
+    }
+    return level;
+  }
+
+  /**
+   * Return default level
+   *
+   * @returns {level}
+   */
+  get defaultLevel() {
+    /**
+     * User can specify own default level value
+     */
+    if (this._settings.defaultLevel) {
+      const userSpecified = this.levels.find((levelItem) => {
+        return levelItem.number === this._settings.defaultLevel;
+      });
+
+      if (userSpecified) {
+        return userSpecified;
+      } else {
+        console.warn(
+          "(ง'̀-'́)ง Heading Tool: the default level specified was not found in available levels"
+        );
+      }
+    }
+
+    /**
+     * With no additional options, there will be H2 by default
+     *
+     * @type {level}
+     */
+    return this.levels[0];
+  }
+
+  /**
+   * @typedef {object} level
+   * @property {number} number - level number
+   * @property {string} tag - tag corresponds with level number
+   * @property {string} svg - icon
+   */
+
+  /**
+   * Available header levels
+   *
+   * @returns {level[]}
+   */
+  get levels() {
+    const availableLevels = [
+      {
+        number: 4,
+        tag: "H4",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="4 7 4 4 20 4 20 7"></polyline><line x1="9" y1="20" x2="15" y2="20"></line><line x1="12" y1="4" x2="12" y2="20"></line></svg>',
+      },
+    ];
+
+    return this._settings.levels
+      ? availableLevels.filter((l) => this._settings.levels.includes(l.number))
+      : availableLevels;
+  }
+
+  /**
+   * Handle H1-H6 tags on paste to substitute it with header Tool
+   *
+   * @param {PasteEvent} event - event with pasted content
+   */
+  onPaste(event) {
+    const content = event.detail.data;
+
+    /**
+     * Define default level value
+     *
+     * @type {number}
+     */
+    let level = this.defaultLevel.number;
+
+    switch (content.tagName) {
+      case "H1":
+        level = 1;
+        break;
+      case "H2":
+        level = 2;
+        break;
+      case "H3":
+        level = 3;
+        break;
+      case "H4":
+        level = 4;
+        break;
+      case "H5":
+        level = 5;
+        break;
+      case "H6":
+        level = 6;
+        break;
+    }
+
+    if (this._settings.levels) {
+      // Fallback to nearest level when specified not available
+      level = this._settings.levels.reduce((prevLevel, currLevel) => {
+        return Math.abs(currLevel - level) < Math.abs(prevLevel - level)
+          ? currLevel
+          : prevLevel;
+      });
+    }
+
+    this.data = {
+      level,
+      text: content.innerHTML,
+    };
+  }
+
+  /**
+   * Used by Editor.js paste handling API.
+   * Provides configuration to handle H1-H6 tags.
+   *
+   * @returns {{handler: (function(HTMLElement): {text: string}), tags: string[]}}
+   */
+  static get pasteConfig() {
+    return {
+      tags: ["H1", "H2", "H3", "H4", "H5", "H6"],
+    };
+  }
+
+  /**
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @returns {{icon: string, title: string}}
+   */
+  static get toolbox() {
+    return {
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="4 7 4 4 20 4 20 7"></polyline><line x1="9" y1="20" x2="15" y2="20"></line><line x1="12" y1="4" x2="12" y2="20"></line></svg>',
+      title: "SubPoints",
+    };
+  }
+}
